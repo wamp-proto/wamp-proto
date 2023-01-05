176d01b0b119d54e1b3f1d8c8f82139fde4604b5
Tobias Oberstein <tobias.oberstein@crossbario.com>
2023-01-05 11:51:30 +0100

    sync

--- _work/ordering-guarantees.py
+++ _work/ordering-guarantees.py
@@ -8,31 +8,45 @@
 #   pip install z3-solver
 
 from z3 import *
-from autobahn.wamp.message import Hello, Welcome, Call, Invocation, Yield, Register, Registered
+from autobahn.wamp.message import Hello, Abort, Goodbye, Welcome, Call, Invocation, Yield, Result, Register, Registered
 from autobahn.wamp.serializer import Serializer
 
 RX = 1
 TX = 2
+
+TR1 = 1
+TR2 = 2
 
 # Message Conversation Log (MCL)
 #
 # The message conversation log records all WAMP messages on
 # all WAMP sessions connected to a router:
 #
-# MCL: Dict(msg_glc, (sess_name, msg_dir, msg_type, ...)
+# MCL: Dict(msg_glc, (msg_trnp, msg_dir, msg_type, <rest of WAMP message>)
 #
-# * sess_name: symbolic session name
+# * msg_trnp: message transport
 # * msg_glc: message send/receive global logical clock
 # * msg_dir: message direction
 # * msg_type: WAMP message type
-# * ...: rest of WAMP message
 #
-MCL = {
+MCL_VALID = {
 
     # HELLO
     # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-26
-    1: ('S1', TX, 1, "realm1", {
-        "agent": "wamp-verifier-client",
+    1: (TR1, TX, Hello.MESSAGE_TYPE, "realm1", {
+        "agent": "wamp-verifier-client1",
+        "roles": {
+            "subscriber": {},
+            "publisher": {},
+            "caller": {},
+            "callee": {},
+        }
+    }),
+
+    # HELLO
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-26
+    2: (TR2, TX, Hello.MESSAGE_TYPE, "realm1", {
+        "agent": "wamp-verifier-client2",
         "roles": {
             "subscriber": {},
             "publisher": {},
@@ -43,8 +57,18 @@ MCL = {
 
     # WELCOME
     # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-28
-    2: ('S1', RX, 2, 9129137332, {
-        "agent": "wamp-verifier-router",
+    3: (TR1, RX, Welcome.MESSAGE_TYPE, 9129137332, {
+        "agent": "wamp-verifier-router1",
+        "roles": {
+            "broker": {},
+            "dealer": {}
+        }
+    }),
+
+    # WELCOME
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-28
+    4: (TR2, RX, Welcome.MESSAGE_TYPE, 5592343255, {
+        "agent": "wamp-verifier-router1",
         "roles": {
             "broker": {},
             "dealer": {}
@@ -53,48 +77,98 @@ MCL = {
 
     # REGISTER
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.1.1-5
-    3: ('S1', TX, 64, 25349185, {}, "com.myapp.echo"),
+    5: (TR1, TX, Register.MESSAGE_TYPE, 25349185, {}, "com.myapp.echo"),
 
     # REGISTERED
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.1.2-5
-    4: ('S1', RX, 65, 25349185, 2103333224),
+    6: (TR1, RX, Registered.MESSAGE_TYPE, 25349185, 2103333224),
 
     # CALL
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.2.1-11
-    5: ('S1', TX, 48, 7814135, {}, "com.myapp.echo", ["Hello, world!"]),
+    7: (TR2, TX, Call.MESSAGE_TYPE, 7814135, {}, "com.myapp.echo", ["Hello, world!"]),
 
     # INVOCATION
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.2.2-11
-    6: ['S1', RX, 68, 6131533, 2103333224, {}, ["Hello, world!"]],
+    8: (TR1, RX, Invocation.MESSAGE_TYPE, 6131533, 2103333224, {}, ["Hello, world!"]),
 
     # YIELD
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.2.3-11
-    7: ['S1', TX, 70, 6131533, {}, ["Hello, world!"]],
+    9: (TR1, TX, Yield.MESSAGE_TYPE, 6131533, {}, ["Hello, world!"]),
 
     # RESULT
     # https://wamp-proto.org/wamp_latest_ietf.html#section-6.2.4-11
-    8: ('S1', RX, 50, 7814135, {}, ["Hello, world!"]),
+    10: (TR2, RX, Result.MESSAGE_TYPE, 7814135, {}, ["Hello, world!"]),
 
     # GOODBYE
     # https://wamp-proto.org/wamp_latest_ietf.html#section-4.2.1-11
-    9: ('S1', TX, 6, {}, "wamp.close.close_realm"),
+    11: (TR1, TX, Goodbye.MESSAGE_TYPE, {}, "wamp.close.close_realm"),
 
     # GOODBYE
     # https://wamp-proto.org/wamp_latest_ietf.html#section-4.2.1-13
-    10: ('S1', RX, 6, {}, "wamp.close.goodbye_and_out"),
+    12: (TR2, RX, Goodbye.MESSAGE_TYPE, {}, "wamp.close.goodbye_and_out"),
 }
 
+
+# invalid: the first message on any transport must be HELLO sent by the client
+MCL_INVALID1 = {
+    # REGISTER
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-6.1.1-5
+    1: (TR1, TX, Register.MESSAGE_TYPE, 25349185, {}, "com.myapp.echo"),
+}
+
+
+# invalid: the first message on any transport must be HELLO sent (!) by the client
+MCL_INVALID2 = {
+    # HELLO
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-26
+    1: (TR1, RX, Hello.MESSAGE_TYPE, "realm1", {
+        "agent": "wamp-verifier-router1",
+        "roles": {
+            "broker": {},
+            "dealer": {}
+        }
+    }),
+}
+
+
+# invalid: the WELCOME must be received after sending HELLO
+MCL_INVALID3 = {
+
+    # WELCOME
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-28
+    1: (TR1, RX, Welcome.MESSAGE_TYPE, 9129137332, {
+        "agent": "wamp-verifier-router1",
+        "roles": {
+            "broker": {},
+            "dealer": {}
+        }
+    }),
+
+    # HELLO
+    # https://wamp-proto.org/wamp_latest_ietf.html#section-4.1.1-26
+    2: (TR1, TX, Hello.MESSAGE_TYPE, "realm1", {
+        "agent": "wamp-verifier-client1",
+        "roles": {
+            "subscriber": {},
+            "publisher": {},
+            "caller": {},
+            "callee": {},
+        }
+    }),
+}
+
+MCLS = [MCL_VALID, MCL_INVALID1, MCL_INVALID2, MCL_INVALID3]
 
 s = Solver()
 
 # the first WAMP message on any peer connection must be HELLO sent by the peer
-first_msg_dir, first_msg_type = Int(MCL[1][1]), Int(MCL[1][2])
+first_msg_dir, first_msg_type = Int(MCL_VALID[1][1]), Int(MCL_VALID[1][2])
 s.add(first_msg_dir == TX, first_msg_type == Hello.MESSAGE_TYPE)
 
 # to do: check for other WAMP protocol rules
 
-for k in sorted(MCL.keys()):
-    evt = MCL[k]
+for k in sorted(MCL_VALID.keys()):
+    evt = MCL_VALID[k]
     sess_name, msg_dir, msg_type = evt[0:3]
     if msg_type == Hello.MESSAGE_TYPE:
         print("Hello")
